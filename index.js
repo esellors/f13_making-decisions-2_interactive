834a59b7bc1582d860ec601db57ed5f4efa19b0d
Eric Sellors <github@esellors.com>
2021-10-28 13:46:16 -0500

    final

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
-console.log('hello world')
+// let backpackItem1 = 'stapler';
+// let backpackItem2 = 'mug';
+// let backpackItem3 = 'pencil';
+// let backpackItem4 = 'extra hat';
+// let backpackItem5 = 'mouse plushie';
+
+// const backpackItems = [];
+const backpackItems = [
+    'stapler',
+    'mug',
+    'pencil',
+    'extra hat',
+    'mouse plushie'
+]
+
+if (backpackItems.length === 0) {
+    backpackItems.unshift('stick')
+}
+
+console.log(backpackItems, backpackItems.length)
+console.log(backpackItems.length + 10)
+
+// array methods
+// backpackItems.push('pillow');
+// backpackItems.push('saw')
+backpackItems.push('pillow', 'saw')
+
+backpackItems.splice(2, 1)
+// const removedItem = backpackItems.splice(3, 1, 'hamster plushie')
+// console.log('I have removed ' + removedItem + '!')
+
+console.log('I have removed ' + backpackItems.splice(3, 1, 'hamster plushie') + '!')
+
+backpackItems.pop();
+
+backpackItems.splice(backpackItems.length - 2)
+
+backpackItems.shift();
+
+
+backpackItems.push('shoes', 'shirt', 'eggplant')
+
+const buddyBackpack = backpackItems.slice(0)
+
+console.log(buddyBackpack)
+console.log(backpackItems)
+
+// for loops and arrays
+const randomItem = backpackItems[1]
+// console.log(randomItem)
+
+console.log(backpackItems)
+
+for (let index = 0; index < backpackItems.length; index++) {
+    // console.log(typeof i, i)
+    // console.log(backpackItems[i])
+    if (backpackItems[index].startsWith('e')) {
+        console.log(backpackItems[index])
+    }
+}
